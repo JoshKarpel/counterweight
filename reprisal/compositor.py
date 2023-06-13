699b371f288b99a87aeee56a35c9ca8a0da0df24
Josh Karpel <josh.karpel@gmail.com>
2023-06-12 23:49:17 -0500

    render a child in a div

--- reprisal/compositor.py
+++ reprisal/compositor.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from textwrap import TextWrapper
 from typing import NamedTuple
 
-from reprisal.elements import Border, Edge, Text
+from typing_extensions import assert_never
+
+from reprisal.elements import Border, Cells, Div, Edge, Text
 
 
 class Position(NamedTuple):
@@ -14,19 +16,11 @@ class Position(NamedTuple):
         return Position(self.x + x, self.y + y)
 
 
-class Span(NamedTuple):
-    width: int
-    height: int
-
-
 class Region(NamedTuple):
     left: int
     right: int
     top: int
     bottom: int
-
-    def span(self) -> Span:
-        return Span(width=self.width, height=self.height)
 
     @property
     def width(self) -> int:
@@ -46,21 +40,73 @@ class Region(NamedTuple):
         return Position(x=self.left, y=self.top)
 
 
+# https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_flow_layout/Block_and_inline_layout_in_normal_flow
+# https://www.w3.org/TR/CSS2/visudet.html#blockwidth
 def compose(
-    element: Text,
+    element: Div | Text,
     region: Region,
 ):
-    margin_comp, inside_margin = edge(edge=element.style.margin, region=region, char=" ")
+    margin_comp, inside_margin = edge(edge=element.style.margin, region=region)
     border_comp, inside_border = border(border=element.style.border, region=inside_margin)
-    padding_comp, inside_padding = edge(edge=element.style.padding, region=inside_border, char=" ")
-    element_comp = text(text=element, region=inside_padding)
+    padding_comp, inside_padding = edge(edge=element.style.padding, region=inside_border)
+
+    if isinstance(element, Text):
+        # this is implicitly like width = "auto" because we use the full horizontal region
+        # what is height implicitly here? not auto because that would shrink, it's more like 100%
+        element_comp = text(text=element, region=inside_padding)
+    elif isinstance(element, Div):
+        element_comp = {}
+        for child in element.children:
+            child_span = child.style.span
+            if child_span.width == "fill":
+                child_width = inside_padding.width
+            elif isinstance(child_span.width, Cells):
+                child_width = child_span.width.cells
+            else:
+                assert_never(child_span.width)
+
+            if child_span.height == "fit":
+                if isinstance(child, Text):
+                    child_height = lines_at_width(child.text, width=child_width) + child.style.box_height()
+                elif isinstance(child, Div):
+                    # how do you drill through the tree here to fit the height?
+                    # I guess the problem with this model is that you kind of need to
+                    # figure out all the widths first, then use those to calculate the heights
+                    # then go back and actually render everything
+                    raise Exception()
+
+            elif child_span.height == "fill":
+                child_height = inside_padding.height
+            elif isinstance(child_span.height, Cells):
+                child_height = child_span.height.cells
+            else:
+                assert_never(child_span.height)
+
+            child_region = Region(
+                left=inside_padding.left,
+                top=inside_padding.top,
+                right=inside_padding.left + child_width,
+                bottom=inside_padding.top + child_height,
+            )
+
+            element_comp |= compose(child, child_region)
+    else:
+        assert_never(element)
 
     return margin_comp | border_comp | padding_comp | element_comp
 
 
+PLACEHOLDER = " ..."
+
+
+def lines_at_width(text: str, width: int) -> int:
+    wrapper = TextWrapper(width=width, max_lines=None, placeholder=PLACEHOLDER)
+    return len(wrapper.wrap(text))
+
+
 def text(text: Text, region: Region):
     chars = {}
-    wrapper = TextWrapper(width=region.width, max_lines=region.height)
+    wrapper = TextWrapper(width=region.width, max_lines=region.height, placeholder=PLACEHOLDER)
     wrapped = wrapper.wrap(text.text)
     for y, line in enumerate(wrapped):
         for x, char in enumerate(line):
--- reprisal/elements.py
+++ reprisal/elements.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum
+from typing import Literal
 
 from pydantic import BaseModel, Extra, Field
 
@@ -12,15 +13,23 @@ class FrozenForbidExtras(BaseModel):
 
 
 class Edge(FrozenForbidExtras):
-    top: int
-    bottom: int
-    left: int
-    right: int
+    top: int = 0
+    bottom: int = 0
+    left: int = 0
+    right: int = 0
+
+    def height(self) -> int:
+        return self.top + self.bottom
+
+    def width(self) -> int:
+        return self.left + self.right
 
 
+# https://www.compart.com/en/unicode/block/U+2500
 class BorderKind(Enum):
     Star = "******"
     Light = "│─┌┐└┘"
+    LightRounded = "│─╭╮╰╯"
     Heavy = "┃━┏┓┗┛"
 
 
@@ -28,10 +37,23 @@ class Border(FrozenForbidExtras):
     kind: BorderKind = Field(default=BorderKind.Light)
 
 
+class Cells(FrozenForbidExtras):
+    cells: int
+
+
+class Span(FrozenForbidExtras):
+    width: Cells | Literal["fill"] = "fill"
+    height: Cells | Literal["fill"] | Literal["fit"] = "fit"
+
+
 class Style(FrozenForbidExtras):
-    margin: Edge | None = Field(default=None)
+    span: Span = Field(default=Span())
+    margin: Edge = Field(default=Edge())
     border: Border | None = Field(default=None)
-    padding: Edge | None = Field(default=None)
+    padding: Edge = Field(default=Edge())
+
+    def box_height(self) -> int:
+        return self.margin.height() + self.padding.height() + 2 if self.border else 0
 
 
 class Div(FrozenForbidExtras):
