1d24f581ba1d966c3c84366e2ea8282e5261041c
Josh Karpel <josh.karpel@gmail.com>
2024-01-01 01:17:21 -0600

    add test for fixed utility

--- tests/styles/test_utilities.py
+++ tests/styles/test_utilities.py
@@ -7,3 +7,7 @@ def test_relative() -> None:
 
 def test_absolute() -> None:
     assert absolute(x=3, y=5) == Style(layout=Flex(position=Absolute(x=3, y=5)))
+
+
+def test_fixed() -> None:
+    assert fixed(x=3, y=5) == Style(layout=Flex(position=Fixed(x=3, y=5)))
