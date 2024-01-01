087e65e401511501e49d97586fad5e819287582f
Josh Karpel <josh.karpel@gmail.com>
2023-12-31 19:43:08 -0600

    changelog

--- docs/_examples/absolute_positioning.py
+++ docs/_examples/absolute_positioning.py
@@ -5,7 +5,7 @@ from counterweight.controls import Quit, Screenshot
 from counterweight.elements import Div, Text
 from counterweight.styles.utilities import *
 
-style = border_heavy | border_bg_blue_800 | pad_1 | padding_amber_500 | margin_1 | margin_red_600
+extra_style = border_heavy | border_bg_blue_800 | pad_1 | padding_amber_500 | margin_1 | margin_red_600
 
 
 @component
@@ -14,7 +14,7 @@ def root() -> Div:
         style=row,
         children=[
             Text(
-                style=absolute(x=x, y=y) | style,
+                style=absolute(x=x, y=y) | extra_style,
                 content=f"absolute(x={x}, y={y})",
             )
             for x, y in (
