73f1ae3ee1e009561b34eaecdb94587d684c6af0
Josh Karpel <josh.karpel@gmail.com>
2024-05-19 17:43:27 -0500

    types

--- counterweight/paint.py
+++ counterweight/paint.py
@@ -430,9 +430,8 @@ def svg(paint: Paint) -> ElementTree:
             },
         )
 
-        for bg_color, x_cells_group in groupby(
-            cells, key=lambda x_cell: x_cell[1].style.background.at(x_cell[0], y=y).hex
-        ):
+        # Assumes resolved colors
+        for bg_color, x_cells_group in groupby(cells, key=lambda x_cell: x_cell[1].style.background.hex):  # type: ignore[union-attr]
             # Optimization: write out long horizontal rectangles of the same background color as rectangles instead of individual cell-sized rectangles
             x_cells = tuple(x_cells_group)
             first_x, first_cell = x_cells[0]
@@ -463,7 +462,8 @@ def svg(paint: Paint) -> ElementTree:
 
                 # optimization: don't write white, it's the default
                 if cell.style.foreground != Color.from_name("white"):
-                    ts.attrib["fill"] = cell.style.foreground.at(x=x, y=y).hex
+                    # Assumes resolved colors
+                    ts.attrib["fill"] = cell.style.foreground.hex  # type: ignore[union-attr]
                 ts.text = cell.char
 
     return ElementTree(element=root)
